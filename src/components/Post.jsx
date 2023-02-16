05e5bd11f186b5a1499c2213c839cd46905be236
hugopradops <hugop.dev@gmail.com>
2023-02-16 15:38:25 -0500

    estilizacao do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,36 @@
-import styles from './Post.module.css'
+import styles from "./Post.module.css";
 
 export function Post() {
-    return (
-        <article className={styles.post}>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/hugopradops.png"
+          ></img>
+          <div className={styles.authorInfo}>
+            <strong> Bob test</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+        <time dateTime="2023-02-06 20:38">Sent 1hr ago</time>
+      </header>
 
-            <header>
-                <div className={styles.author}>
-                    <img
-                        className={styles.avatar}
-                        src="https://github.com/hugopradops.png">
-                    </img>
-                    <div className={styles.authorInfo}>
-                        <strong> Bob test</strong>
-                        <span>Web developer</span>
-                    </div>
-                </div>
-                <time dateTime='2023-02-06 20:38'>Sent 1hr ago</time>
-            </header>
-
-            <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉<a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
-            </div>
-        </article>
-    );
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
 }
